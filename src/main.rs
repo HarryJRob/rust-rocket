a82e1bfff712a68d0c23af45d0f4c9ac9ed9a0cd
Harry Robertson <harryjrobertson@gmail.com>
2021-04-04 22:03:37 +0100

    Implemented the template engine

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
+use rocket_contrib::templates::Template;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context = ();
+    Template::render("index", &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
